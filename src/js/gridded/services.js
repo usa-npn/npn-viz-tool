ad9f35c35235b5ea719e6e1f6747b56102adf755
Paul Adams <adamspe@gmail.com>
2016-03-24 14:54:00 -0500

    increase the grid size to send to the wcs server from 4 to 5

--- src/js/gridded/services.js
+++ src/js/gridded/services.js
@@ -1123,7 +1123,7 @@ angular.module('npn-viz-tool.gridded-services',[
              * @return {promise} A promise that will be resolved with an array of numbers, or rejected.
              */
             getGriddedData: function(latLng) {
-                return WcsService.getGriddedData(GEOSERVER_URL,this,latLng,4/*should gridSize change based on the layer?*/);
+                return WcsService.getGriddedData(GEOSERVER_URL,this,latLng,5/*should gridSize change based on the layer?*/);
             }
         });
         return l;
